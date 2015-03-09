bc460b20a31fc53dc27860964390dcbb03517e36
huzhi <huzhi@sogou-inc.com>
2015-03-09 18:16:22 +0800

    一半的架构

--- app/src/main/java/com/xiaomei/contanier/TabsActivity.java
+++ app/src/main/java/com/xiaomei/contanier/TabsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.xiaomei.BaseActiviy;
+import com.xiaomei.R;
 
 /**
  * Created by huzhi on 15-2-17.
@@ -20,6 +21,7 @@ public class TabsActivity extends BaseActiviy{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container_layout);
     }
 
 }
